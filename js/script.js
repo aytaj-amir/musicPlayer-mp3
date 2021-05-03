96567f1ad3a1e3c60d2d458aafe47f0f15e1462d
aytaj-amir <aytac.amirova1@gmail.com>
2021-05-04 02:54:16 +0400

    files

---
+++ js/script.js
@@ -0,0 +1,124 @@
+const musicContainer=document.querySelector('.music-container');
+const playBtn=document.querySelector('#play');
+const prevBtn=document.querySelector('#prev');
+const nextBtn=document.querySelector('#next');
+const audio=document.querySelector('#audio');
+const progress=document.querySelector('.progress');
+const progressContainer=document.querySelector('.progress-container');
+const title=document.querySelector('#title');
+const cover=document.querySelector('#cover');
+
+
+
+// Song titles
+const songs=['Charlie Puth - We Don`t Talk Anymore feat. Selena Gomez','Charlie Puth- Attention','David Guetta, Bebe Rexha ft J Balvin - Say My Name Official Video']
+
+
+// Keep track of songs
+
+let songIndex=2
+
+//Initially  load songs into DOM
+
+loadSong(songs[songIndex])
+
+//Upload song details
+
+function loadSong(song){
+	title.innerText = song
+	audio.src=`music/${song}.mp3`
+	cover.src=`image/${song}.png`
+}
+
+
+function playSong(){
+
+	musicContainer.classList.add('play')
+	playBtn.querySelector('i.fas').classList.remove('fa-play')
+	playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+	audio.play()
+}
+
+
+function pauseSong(){
+	musicContainer.classList.remove('play')
+	playBtn.querySelector('i.fas').classList.add('fa-play')
+	playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+	audio.pause()
+}
+
+
+function prevSong(){
+	songIndex--
+
+	if(songIndex < 0){
+		songIndex = songs.length - 1
+	}
+
+	loadSong(songs[songIndex])
+
+	playSong()
+}
+
+
+function nextSong(){
+	songIndex++
+
+	if(songIndex > songs.length - 1){
+		songIndex = 0
+	}
+
+	loadSong(songs[songIndex])
+
+	playSong()
+}
+
+
+
+function updateProgress(e){
+
+// console.log(e.srcElement.currentTime); mahninin saniyelerini gosterir
+// console.log(e.srcElement.duration);
+
+const {duration,currentTime} = e.srcElement
+const progressPercent = (currentTime/duration) * 100
+progress.style.width=`${progressPercent}%`
+
+
+
+}
+
+
+
+function setProgress(e){
+	const width=this.clientWidth
+	const clickX= e.offsetX
+	const duration= audio.duration
+	// console.log(clickX)
+
+	audio.currentTime=( clickX / width) * duration
+}
+
+//Event listener
+
+playBtn.addEventListener('click',()=>{
+	const isPlaying=musicContainer.classList.contains('play')
+
+	if(isPlaying){
+		pauseSong()
+	}else{
+		playSong()
+	}
+})
+
+
+//Change song events
+
+prevBtn.addEventListener('click',prevSong)
+nextBtn.addEventListener('click',nextSong)
+audio.addEventListener('timeupdate',updateProgress)
+
+progressContainer.addEventListener('click',setProgress)
+audio.addEventListener('ended',nextSong)
